f79c8836f98ce3a070f7b9f23756be11f2b834b0
geduramc <geduradev@gmail.com>
2023-06-22 01:20:31 -0500

    feat: add rpi_init.sh script to run on boot rpi

---
+++ shell/rpi_init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MY_IP_SCRIPT="~/workspace/scripts/myip.py"
+
+echo "Init rpi0 - $(date)"
+
+echo "Start notificator-api:"
+pm2 resurrect
+pm2 start notificator-api
+sleep 5
+
+echo "Send my ip:"
+python $MY_IP_SCRIPT
+
+exit
